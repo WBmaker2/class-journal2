c1692fa8158c31e28b4ee341cd76e70c21530535
ketarou85 <ketarou85@gmail.com>
2026-02-10 11:51:29 +0900

    Update OAuth setup instructions in SettingsManager

--- src/features/SettingsManager.tsx
+++ src/features/SettingsManager.tsx
@@ -163,6 +163,7 @@ export const SettingsManager: React.FC = () => {
                     Google Cloud Console <ExternalLink size={12} />
                   </a>에 접속합니다. 새 프로젝트를 생성합니다.
                 </li>
+                <li>[API 및 서비스] &gt; [OAuth 동의 화면]으로 이동합니다. User Type '외부'선택 후 '만들기'를  클릭합니다.</li>
                 <li>[API 및 서비스] &gt; [사용자 인증 정보]로 이동합니다.</li>
                 <li><strong>OAuth 2.0 클라이언트 ID</strong>를 생성하고, 클릭하여 수정 화면으로 들어갑니다.</li>
                 <li>
@@ -172,6 +173,7 @@ export const SettingsManager: React.FC = () => {
                   <span className="text-xs text-blue-600">(마지막에 '/'가 없어야 합니다)</span>
                 </li>
                 <li>저장 후 <strong>Client ID</strong>를 복사하여 아래에 입력하세요.</li>
+                <li><a hred="https://goldenrabbit.co.kr/articles/p4esg2wuouyO0dTyDYGG">OAuth 2.0 클라이언트 ID 발급 방법 참고 자료</a> 승인된 자바스크립트 원본 주소는 꼭 위 안내된 주소를 입력하세요. 승인된 리디렉션 URI는 비워둡니다.</li>
               </ol>
             </div>
           </div>
