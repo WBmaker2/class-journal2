696980651d316ff23826ba09b6e453de48711a77
Kim Hongnyeon <kimhongnyeon@Kimui-Macmini.local>
2026-03-20 21:55:00 +0900

    refactor: update provider hierarchy in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { JournalProvider } from './context/JournalContext'
-import { SupabaseProvider } from './context/SupabaseContext'
+import { AuthProvider } from './context/AuthContext'
+import { SecurityProvider } from './context/SecurityContext'
+import { SyncProvider } from './context/SyncContext'
 import { ToastProvider } from './context/ToastContext'
 import { ClassProvider } from './context/ClassContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
-      <SupabaseProvider>
-        <ClassProvider>
-          <JournalProvider>
-            <App />
-          </JournalProvider>
-        </ClassProvider>
-      </SupabaseProvider>
+      <AuthProvider>
+        <SecurityProvider>
+          <SyncProvider>
+            <ClassProvider>
+              <JournalProvider>
+                <App />
+              </JournalProvider>
+            </ClassProvider>
+          </SyncProvider>
+        </SecurityProvider>
+      </AuthProvider>
     </ToastProvider>
   </StrictMode>,
 )
